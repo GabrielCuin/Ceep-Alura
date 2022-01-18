367463b5ffb9a94691e856e4056839dc732cc1cc
Gabriel Cuin dos Santos <gabrielcuinprof@gmail.com>
2022-01-18 15:03:08 -0300

    Novas função implementadas com o react

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -4,51 +4,27 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 import ListaDeCategorias from "./components/ListaDeCategorias";
-import { toBeInTheDocument } from "@testing-library/jest-dom";
+import Categorias from './dados/Categorias';
+import ArrayDeNotas from "./dados/Notas";
 class App extends Component {
 
   constructor(){
     super();
-
-    this.state = {
-      notas:[],
-      categorias: [],
-    }
-  }
-
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas,novaNota]
-    const novoEstado = {
-      notas:novoArrayNotas
-    }
-    this.setState(novoEstado)
-  }
-
-  adicionarCategoria(nomeCategoria) {
-    const novoArrayCategoria = [...this.state.categorias, nomeCategoria]
-    const novoEstado = {...this.state, categorias:novoArrayCategoria}
-    this.setState(novoEstado)
-  }
-
-  deletarNota(index) {
-    let arrayDeNotas = this.state.notas;
-    arrayDeNotas.splice(index, 1);
-    this.setState({notas:arrayDeNotas});
-    console.log("Deletar");
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <FormularioCadastro categorias={this.categorias} criarNota={this.notas.adicionarNota.bind(this.notas)}/>
         <main className="conteudo-pricipal">
           <ListaDeCategorias 
-          adicionarCategoria={this.adicionarCategoria.bind(this)}
-          categorias={this.state.categorias} />
+          adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+          categorias={this.categorias} />
           <ListaDeNotas
-            apagarNota={this.deletarNota.bind(this)}
-            notas={this.state.notas}/>    
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+            notas={this.notas}/>    
         </main>
       </section>
     );
